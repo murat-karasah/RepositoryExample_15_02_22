a1f898e03a4c8f629c21159c683ff40e1d91d469
Murat Karasah <mkarasah@gmail.com>
2022-02-15 11:45:32 +0300

    Add project files.

---
+++ RepositoryExample_15_02_22/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using RepositoryExample_15_02_22.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RepositoryExample_15_02_22.Controllers
+{
+    public class CategoryController : Controller
+    {
+        BaseRepository<Categories> rep = new BaseRepository<Categories>();
+        CategoriesModel cm = new CategoriesModel();
+
+        public ActionResult Index(string name)
+        {
+            if (name==null)
+            {
+                name = "";
+            }
+            //cm.cList = rep.Listele();
+            cm.cList = rep.GenelListe().Where(x => x.CategoryName.Contains(name)).ToList();
+            return View(cm);
+        }
+        public ActionResult Detay(int id)
+        {
+            cm.Categories = rep.Bul(id);
+            return View(cm);
+        }
+
+        public ActionResult Ekle()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekle(CategoriesModel cm)
+        {
+            if (ModelState.IsValid)
+                {
+                Categories c = new Categories();
+                c.CategoryID = cm.Categories.CategoryID;
+                c.CategoryName = cm.Categories.CategoryName;
+                c.Description = cm.Categories.Description;
+                rep.Ekle(c);
+                rep.Guncel();
+                return RedirectToAction("Index");
+                }
+            return View();
+        }
+
+    }
+}
---
+++ RepositoryExample_15_02_22/Models/CategoriesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepositoryExample_15_02_22.Models
+{
+    public class CategoriesModel
+    {
+        public List<Categories> cList { get; set; }
+        public Categories Categories { get; set; }
+    }
+}
